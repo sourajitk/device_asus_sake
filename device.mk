69c10df911a7704bcf848e03573f3a39b0419dca
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-05-07 16:45:50 -0400

    sake: Copy over audio policy config to sku_lahaina for proper detection
    Without the configuration file being in the right place, wired headsets
    for example, stop working. We can check using dumpsys that it works fine
    now:
    
      - Route 3:
        - Type: Mix
        - Sink: Wired Headset
        - Sources:
            primary output
            deep_buffer
            direct_pcm
            compressed_offload
            voip_rx
            mmap_no_irq_out
            haptics output
            dsd_compress_passthrough
    
      - Route 4:
        - Type: Mix
        - Sink: Wired Headphones
        - Sources:
            primary output
            deep_buffer
            direct_pcm
            compressed_offload
            voip_rx
            mmap_no_irq_out
            haptics output
            dsd_compress_passthrough
    
    Change-Id: I6fbc8bbf41cca156f174ae56f622f102a73f58cf
    Reviewed-on: https://review.statixos.com/c/android_device_asus_sake/+/7675
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>

--- device.mk
+++ device.mk
@@ -48,7 +48,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/audio_io_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/audio_platform_info_intcodec.xml \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina_qssi/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/audio_policy_volumes.xml \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/default_volume_tables.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/mixer_paths.xml \
