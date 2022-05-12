9f421156f913ececfc3f3d445fc1539a775dd758
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-05-11 21:00:24 -0400

    Revert "sake: Build Pixel Launcher"
    This reverts commit 0872fa12edac060c42f415e74f9f58c57e97fc54.
    
    Change-Id: I3e4c114a267bdb936c023a524d1f06f72d10dc37
    Reviewed-on: https://review.statixos.com/c/android_device_asus_sake/+/7714
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>

--- device.mk
+++ device.mk
@@ -4,9 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Pixel Launcher
-INCLUDE_PIXEL_LAUNCHER := true
-
 # Inherit from our proprietary files directory.
 $(call inherit-product, vendor/asus/sake/sake-vendor.mk)
 
