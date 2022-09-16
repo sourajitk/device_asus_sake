35b042ef29026f6e40e9980dd093bc22633214d0
Sourajit Karmakar <sourajitk@statixos.com>
2022-09-16 19:29:07 -0400

    sake: Move to the adreno component
    Change-Id: I50d4d162edc80a9ec9369566aed77c95148d9497

--- device.mk
+++ device.mk
@@ -31,15 +31,6 @@ PRODUCT_PACKAGES += \
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Adreno
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
-    frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
-    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
-
 # Audio
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8350/audio
 
@@ -325,6 +316,10 @@ PRODUCT_PACKAGES += \
 
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Qualcomm Components
+TARGET_COMMON_QTI_COMPONENTS := \
+    adreno
 
 # QMI
 TARGET_FWK_SUPPORTS_FULL_VALUEADDS := true
