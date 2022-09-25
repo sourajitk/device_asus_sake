bd139cb766d9061b5e9d26f21c823fc4bec10b94
Lunarixus <Nathan@lunarixus.dev>
2022-09-25 12:44:07 -0400

    sake: Set surfaceflinger blurs as expensive
    * Lowers blur quality and uses EXPENSIVE_RENDERING powerhints.
    
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>
    Change-Id: I8c8455e36416cdc5acdd25925444efd5a70dbb72

--- device.mk
+++ device.mk
@@ -176,6 +176,7 @@ PRODUCT_PACKAGES += \
     lights.qcom
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.supports_background_blur=1
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.sf.blurs_are_expensive=1
 
 # Fastboot
 PRODUCT_PACKAGES += \
